57f616a27a0a3af39c6826be1ff0575c35733de8
bergmant <tommi.bergman@fmi.fi>
2026-08-18 15:12:26 +0300

    explicit arguments added becaus GCC15 requires these

--- ifs-source/ifsaux/support/bbt.h
+++ ifs-source/ifsaux/support/bbt.h
@@ -15,8 +15,8 @@
 #define _BBT_H
 
 
-void bbt_insert_bbt(void *, void *, int (*)());
-void bbt_delete_bbt(void *, void *, int (*)());
+void bbt_insert_bbt(void *, void *, int (*)(const void *, const void *));
+void bbt_delete_bbt(void *, void *, int (*)(const void *, const void *));
 
 #define BBT_HEADER(self) int priority; struct self *left, *right
 
